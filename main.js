b8590517443262cc54c40c002daa9b948254f252
ajiaja38 <ajicooljazz38@gmail.com>
2025-12-12 10:24:06 +0700

    fix: update cors :zap:

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const app = express()
 const port = 3000
 const prefix = "/api"
 
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
